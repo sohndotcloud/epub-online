45b59c40eab2dc9d22ad3585757a0005d7da5a3f
Nils Sohn <sohn.cloud@proton.me>
2025-09-12 18:48:39 -0700

    Added animation for the page header

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,18 @@ export default function Home() {
   return (
     <div>
     <div className="h-10 w-full flex dark:bg-neutral-900">
-        <motion.h1
-
+        <motion.div
           initial={{ opacity: 0, y: -25}}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1 }}
-          className="mt-2 w-full text-center md:pl-10 w-50 font-mono font-extrabold text-l p-5 text-[20px] mt-2 lg:text-[24px] w-80 dark:text-amber-300"><Link href="/">Nils Sohn</Link></motion.h1>
+          className="mt-2 w-full text-center md:pl-10 w-50 font-mono font-extrabold text-l p-5 text-[20px] mt-2 lg:text-[24px] w-80 dark:text-amber-300"><Link href="/">Nils Sohn <motion.div animate={{ rotate: -15 }}
+            transition={{ duration: .5 }}>
+          <motion.div animate={{ rotate: 30 }}
+            transition={{ delay: .5, duration: .5 }}>
+              <motion.div animate={{ opacity: 0 }}
+            transition={{ delay: 1, duration: .2 }}>
+              👋</motion.div></motion.div></motion.div>
+              </Link></motion.div>
       <ul className="hidden md:flex w-200 pl-14 pr-14 mt-3 justify-between p-5 lg:justify-center item-center w-full mt-3 dark:text-amber-300">
         <motion.li initial={{ opacity: 0, x: +25}}
           animate={{ opacity: 1, x: 0 }}
