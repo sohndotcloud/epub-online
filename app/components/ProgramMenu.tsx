8791a0ce8501a224403e8184d75ffc628c71c28c
Nils Sohn <sohn.cloud@proton.me>
2025-09-13 02:31:10 -0700

    Added options for menu to do different feature updates in the home page

--- app/components/ProgramMenu.tsx
+++ app/components/ProgramMenu.tsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const ProgramMenu = () => {
 
-  return (
+    const menuItems = [ "Bubble Theme", "Tail Theme", "Portfolio Theme", "Contact Theme"];
+    const [selectedIndex, setSelectedIndex] = useState(0);
+
+    return (
     <div id="menu" className="pl-5">
-        <div id="home" className="bg-amber-50 text-amber-950 font-extrabold">Home</div>
-        <div id="about">About</div>
-        <div id="portfolio">Portfolio</div>
-        <div id="contact">Contact</div>
+        {menuItems.map((item, index) => (
+            <div
+                key={index}
+                className={`${selectedIndex === index ? 'bg-amber-50 text-amber-950' : ''}`} onClick={() => setSelectedIndex(index)} >
+                    {item}
+                    </div>
+        ))}
     </div>
-  )
+    )
 }
 
 export default ProgramMenu
